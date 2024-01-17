c283b478e8fef721351c822e0ffcd13aaa5c02e2
Piotr Szczypior <piotr.szczypior@capgemini.com>
2024-01-17 23:52:09 +0100

    Students taught by teacher and grades of students

---
+++ src/app/teacher/service/student-table.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {Student, StudentItem} from '../model/Student';
+import {Router} from '@angular/router';
+import {BehaviorSubject, Observable} from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentTableService {
+
+  private tableItemsSubject$: BehaviorSubject<StudentItem[]>;
+
+  constructor(private router: Router) {
+  }
+
+  public getTableItems(students: Student[]): Observable<StudentItem[]> {
+    const tableItems = students.map(student => this.mapToTableItem(student));
+    this.tableItemsSubject$ = new BehaviorSubject<StudentItem[]>(tableItems);
+    return this.tableItemsSubject$.asObservable();
+  }
+
+  private mapToTableItem(student: Student): StudentItem {
+    return {
+      data: student,
+      action: () => this.router.navigate([`teacher/${student.id}/grades`]),
+    }
+  }
+}
